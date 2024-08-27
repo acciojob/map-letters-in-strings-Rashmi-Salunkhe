0b59347eb1abf848a5876f30c0ed0c9cd0cd0868
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-08-27 15:56:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+function mapLetters(word) {
+    const result = {};
+
+    for (let i = 0; i < word.length; i++) {
+        const letter = word[i];
+        if (result[letter]) {
+            result[letter].push(i);
+        } else {
+            result[letter] = [i];
+        }
+    }
+
+    return result;
+}
+
+// DO NOT change the code below
+const word = prompt("Enter a word:");
+alert(JSON.stringify(mapLetters(word)));
